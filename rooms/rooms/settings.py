0f9f319f92ed714991f15001803188d865bd044f
Christian Carlowitz <cc@cmesh.de>
2020-11-25 23:56:56 +0100

    fixed secretkey.txt path issue

--- rooms/rooms/settings.py
+++ rooms/rooms/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY_FILE = "secretkey.txt"
+SECRET_KEY_FILE = BASE_DIR / "secretkey.txt"
 if isfile(SECRET_KEY_FILE):
     SECRET_KEY = open(SECRET_KEY_FILE, 'r').read()
 else:
