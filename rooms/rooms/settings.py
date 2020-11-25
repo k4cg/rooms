5848e9240e24c846abd5791d231a3d480e0153e1
Christian Carlowitz <cc@cmesh.de>
2020-11-26 00:05:07 +0100

    added missing full path for template dir

--- rooms/rooms/settings.py
+++ rooms/rooms/settings.py
@@ -65,7 +65,7 @@ ROOT_URLCONF = 'rooms.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [ BASE_DIR / 'templates' ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
